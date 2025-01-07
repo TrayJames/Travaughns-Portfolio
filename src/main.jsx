ec11f80e879a189364407890634973b5f2614013
TrayJames <exscalibar@hotmail.com>
2025-01-07 08:46:59 -0500

    It works! But test4

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+//import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/Travaughns-Portfolio/">
+    {/* <BrowserRouter basename="/Travaughns-Portfolio/">
       <Routes>
         <Route path="*" element={<App />}/>
       </Routes>
-    </BrowserRouter>
-    {/* <App /> */}
+    </BrowserRouter> */}
+    <App />
   </StrictMode>
 )
